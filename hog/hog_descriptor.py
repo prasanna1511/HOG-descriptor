52384d1a31ef68f152e6e939acfc8e878ba848e3
prasanna1511 <53254596+prasanna1511@users.noreply.github.com>
2024-09-11 18:56:05 +0200

    Update hog_descriptor.py

--- hog/hog_descriptor.py
+++ hog/hog_descriptor.py
@@ -15,18 +15,11 @@ def compute_magnitude(img: np.ndarray) -> np.ndarray:
     """
 
     magnitude = np.zeros((img.shape[0], img.shape[1]))
-
-    #compute each pixels magnitude
-    for i in range (1,img.shape[0]-1):
-        for j in range(1,img.shape[1]-1):
-            gx_ij =img[i,j+1] -img[i,j-1]
-            gy_ij =img[i+1,j] -img[i-1,j]
-            magnitude[i,j]=np.sqrt(gx_ij**2 +gy_ij**2)
-
-            #print first 10values
-            # print(magnitude[:10])
-
-        
+    for i in range (1, img.shape[0]-1):
+        for j in range(1, img.shape[1]-1):
+            gx_ij = img[i, j+1] - img[i, j-1]
+            gy_ij = img[i+1, j] - img[i-1, j]
+            magnitude[i,j] = np.sqrt(gx_ij**2 + gy_ij**2)
 
     return magnitude
 
@@ -42,18 +35,14 @@ def compute_angle(img: np.ndarray) -> np.ndarray:
     """
 
     angle = np.zeros((img.shape[0], img.shape[1]))
-
-    #compute each pixels angle
-    for i in range(1,img.shape[0]-1):
-        for j in range(1,img.shape[1]-1):
-            gx_ij =img[i,j+1] - img[i,j-1]
-            gy_ij = img[i+1,j] - img[i-1, j]
-            angle[i,j] = np.abs(np.arctan2(gy_ij,gx_ij))
-            angle[i,j] =np.rad2deg(angle[i,j])
-
-            # print(angle[:5])
-
-
+    for i in range(1, img.shape[0]-1):
+        for j in range(1, img.shape[1]-1):
+            gxij = img[i, j+1] - img[i, j-1]
+            gyij = img[i+1, j] - img[i-1, j]
+            angle[i,j] = np.abs(np.arctan2(gyij, gxij))
+    # print(angle[:5]), print(angle[-5:])
+            angle[i,j] = np.rad2deg(angle[i,j])
+    # print(angle[:5]), print(angle[-5:])
     return angle
 
 def create_cell_histograms(mag_cells: np.ndarray, ang_cells: np.ndarray, bin_size: Optional[int] = 9) -> np.ndarray:
@@ -71,25 +60,14 @@ def create_cell_histograms(mag_cells: np.ndarray, ang_cells: np.ndarray, bin_siz
 
     bins = np.zeros((mag_cells.shape[0], mag_cells.shape[1], bin_size))
     step_size = 180 / bin_size
-    #implement the angles histogram for each cell
 
-    
-
-
-    #compute the histogram for each cell
     for i in range(mag_cells.shape[0]):
         for j in range(mag_cells.shape[1]):
-            # iterate over the angle cells
             for k in range(ang_cells.shape[2]):
-            
-                # compute the range of each bin (0-20, 20-40, etc.)\
-                #bin =step_size*k, step_size*(k+1)
-                bin_range = [step_size * k, step_size * (k + 1)]
-                # find the indices of the pixels that fall within this range
-                indices = np.where((ang_cells[i, j, :] >= bin_range[0]) & (ang_cells[i, j, :] < bin_range[1]))
-                # sum the magnitudes of these pixels
-                bins[i, j, k] = np.sum(mag_cells[i, j, indices])
-                      
+                bin_range = [k*step_size, (k+1)*step_size]
+                indices = np.where((ang_cells[i,j,:]>= bin_range[0]) & (ang_cells[i,j,:] < bin_range[1]))
+                bins[i,j,k] = np.sum(mag_cells[i,j,indices])
+
 
     return bins
 
@@ -114,21 +92,14 @@ def dataloader(data_files: np.ndarray) -> np.ndarray:
         # - hist_cells should be output histogram from create_cell_histograms function
         ###########################################
 
-        #compute the magnitude and angle for each pixel
-        magnitude = compute_magnitude(img)
-        angle = compute_angle(img)
+        mag = compute_magnitude(img)
+        ang = compute_angle(img)
 
-        #divide each image into 8x8 cells
         num_cells = img.shape[0]//8, img.shape[1]//8
-        mag_cells =magnitude[:num_cells[0]*8, :num_cells[1]*8].reshape((num_cells[0], 8, num_cells[1], 8)).transpose((0, 2, 1, 3))
-        ang_cells =angle[:num_cells[0]*8, :num_cells[1]*8].reshape((num_cells[0], 8, num_cells[1], 8)).transpose((0, 2, 1, 3))
+        mag_cells = mag[:num_cells[0]*8, :num_cells[1]*8].reshape((num_cells[0], 8, num_cells[1], 8)).transpose((0,2,1,3))
+        ang_cells = ang[:num_cells[0]*8, :num_cells[1]*8].reshape((num_cells[0], 8, num_cells[1], 8)).transpose((0,2,1,3))
 
-        
-        
-
-        #compute the histogram for each cell
-        hist_cells = create_cell_histograms(mag_cells, ang_cells, bin_size=9)
-
+        hist_cells = create_cell_histograms(mag_cells, ang_cells,8)
 
         ###########################################
 
